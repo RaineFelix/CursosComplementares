a2ed5b89c62b6793e02435ece4c2db3a660698e6
Raíne Felix <103448674+RaineFelix@users.noreply.github.com>
2023-01-03 20:41:42 -0300

    Delete Curso Dev Aprender directory

--- Curso Dev Aprender/Modulo05_Arrays/script.js
+++
@@ -1,5 +0,0 @@
-//Tipos de referência - ARRAYS
-
-let pessoa = ['raine', 18, 'Mogi Guacu' ]
-console.log(pessoa.length)
-console.log(pessoa)
--- Curso Dev Aprender/Modulo11_While/script.js
+++
@@ -1,9 +0,0 @@
-// While
-
-//Declarar a variável de indice fora do loop
-
-let i = 5
-
-while (i>=1) {
-    console.log(i+1 +'º Estou aprendendo')
-}
